338f1d313d09732e50f30480f6d642297cbfbd30
monmonmon175@yahoo.com <monmonmon175@yahoo.com>
2020-06-21 03:16:15 +0200

    Updates

---
+++ precache-manifest.025de2e72333deab29c4f12b347eb05f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9040a620282f459737cfb4a264468c11",
+    "url": "/MarkDown-Previewer/index.html"
+  },
+  {
+    "revision": "3b710e6e748bc316d82d",
+    "url": "/MarkDown-Previewer/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "56da8e76016a41dddc9e",
+    "url": "/MarkDown-Previewer/static/css/main.f121aef6.chunk.css"
+  },
+  {
+    "revision": "3b710e6e748bc316d82d",
+    "url": "/MarkDown-Previewer/static/js/2.2cb3bbd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MarkDown-Previewer/static/js/2.2cb3bbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56da8e76016a41dddc9e",
+    "url": "/MarkDown-Previewer/static/js/main.662224d2.chunk.js"
+  },
+  {
+    "revision": "c32b3ed33c477bc73cd5",
+    "url": "/MarkDown-Previewer/static/js/runtime-main.5fe9b520.js"
+  }
+]);
